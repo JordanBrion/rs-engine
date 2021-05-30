ca6a1fab2bb637800a4f7d07dd3da814af53eebc
Jordan BRION <jrd.brion@gmail.com>
2021-05-30 10:53:08 +0200

    window.rs: define window struct

---
+++ src/window.rs
@@ -0,0 +1,29 @@
+use crate::renderingcontext::*;
+use sdl2::*;
+
+struct MyWindow {
+    width: usize,
+    height: usize,
+    inner: sdl2::video::Window,
+}
+
+impl MyWindow {
+    pub fn new(context: &MyRenderingContext) -> Result<MyWindow, &'static str> {
+        let width = 1280;
+        let height = 720;
+        let window = context
+            .sdl_context
+            .video()
+            .unwrap()
+            .window("rust-sdl2 demo", width, height)
+            .vulkan()
+            .position_centered()
+            .build()
+            .unwrap();
+        Ok(MyWindow {
+            width: width as usize,
+            height: height as usize,
+            inner: window,
+        })
+    }
+}
